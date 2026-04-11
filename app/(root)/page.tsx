c98fd3c1da033f845b69b87c46d233e49d3fcedd
rahul sharma <rraj25198@gmail.com>
2026-04-11 19:20:49 +0530

    feat : implement google auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+export default async function Home() {
+  
   return (
-    <div className="h-full text-white flex-center">
+    <div className="flex-center h-full text-white">
       <h1 className="text-4xl font-bold text-orange-600">Welcome to Home</h1>
+      
+      <form
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Logout</Button>
+      </form>
     </div>
   );
 }
