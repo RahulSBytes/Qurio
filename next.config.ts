8815c85d699e9f2f98d9e2361f8cccd6aac77030
rahul sharma <rraj25198@gmail.com>
2026-06-29 08:07:51 +0530

    configure log in with credential

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages : ["pino", "pino-pretty"],
+  serverExternalPackages: ["pino", "pino-pretty"],
   images: {
-    domains: ["i.pravatar.cc"],
-  },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "*",
+        port: "",
+      },
+    ],
+  }
 };
 
 export default nextConfig;
